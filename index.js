88758b3f64620e3c73f13f58c71ed8267fceff5a
Hoàng Anh <hoanganhlclqd@gmail.com>
2023-02-02 21:10:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const unfluff = require("unfluff");
 const axios = require("axios");
 const cors = require("cors");
 const PORT = process.env.PORT || 3001;
+const summary = require("./Handler/summarize")
+const localExtractor = require("./Handler/extractor")
+var fs = require("fs");
 
 app.use(cors());
 
@@ -10,17 +13,38 @@ app.listen(PORT, () => {
   console.log("listing on port", PORT);
 });
 
-app.get("/", (req, res) => {
+app.post("/", (req, res) => {
   res.status(200).send("Hello world 🌏");
 });
 
-app.get("/content", async (req, res) => {
+app.post("/content", async (req, res) => {
   try {
-    const url = req.query.url;
-    const response = await axios.get(url);
-    const data = unfluff(response.data);
-    res.status(200).send(data.text);
-  } catch (e) {
+    const num = req.query.params.split(" ")[1];
+    const type = req.query.params.split(" ")[0];
+    var article;
+    if (type == "viaLink") {
+      const url = req.query.url;
+      const response = await axios.get(url);
+      const data = unfluff(response.data, 'en');
+      article = data.text;
+    }
+    if (type == "local") {
+      article = req.query.data;
+    }
+    var numOfSentences = localExtractor.numOfSentences(article);
+    if (num == undefined){
+      var sumArticle = summary.summarize(article, numOfSentences);
+      res.status(200).send(sumArticle);
+    } 
+    else if (num <= numOfSentences) {
+      var sumArticle = summary.summarize(article, num);
+      res.status(200).send(sumArticle);
+    }
+    else {
+      res.status(200).send("UNABLE TO SUMMARIZE TO GIVEN NUMBER OF SENTENCES. MINIMUM NUMBER OF SENTENCES CAN BE SUMMARIZED TO IS "+  (numOfSentences) +".")
+    }
+  }
+  catch (e) {
     res.status(400).send(e);
   }
 });
