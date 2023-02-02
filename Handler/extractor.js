36a82b1462a81c5ca3e56710950d01a8d1e8f3f7
Hoàng Anh <hoanganhlclqd@gmail.com>
2023-02-02 21:29:22 +0700

    push

---
+++ Handler/extractor.js
@@ -0,0 +1,45 @@
+var fs = require("fs");
+var parser = require("./parser");
+var filter = require("./filter");
+
+function numOfSentences(sentence) {
+    // xử lý dữ liệu nhập vào trả về list các câu
+    var sentences = getSentences(sentence);
+    sentences = filter.omitTransitionSentences(sentences);
+    return sentences.length;
+}
+
+function getSentences(sentence) {
+    // xử lý dữ liệu nhập vào trả về list các câu
+    sentence = sentence.replaceAll("\n", " ");
+    sentence = parser.removeNumTag(sentence);
+    sentence = parser.convertAbbreviations(sentence);
+    sentence = sentence.replaceAll("?", ".");
+    sentence = sentence.replaceAll("!", ".");
+    var sentences = sentence.split(". ");
+    sentences = parser.fixBrokenSentences(sentences);
+    sentences = parser.removeWhiteSpaceList(sentences);
+    sentences = parser.removeBlanks(sentences);
+    sentences = parser.addPeriods(sentences);
+    sentences = parser.cleanupQuotes(sentences);
+    sentences = parser.groupQuotes(sentences);
+    sentences = parser.commaHandler(sentences);
+    return sentences;
+}
+
+function getWords(word) {
+    // xử lý dữ liệu nhập vào trả về list các từ
+    word = word.replaceAll("\n", " ");
+    word = parser.convertAbbreviations(word);
+    var words = word.split(" ");
+    words = parser.removeBlanks(words);
+    for (var i = 0; i < words.length; i++)
+        words[i] = parser.cleanWord(words[i]);
+    return words;
+}
+
+module.exports = {
+    getWords,
+    getSentences,
+    numOfSentences,
+};
